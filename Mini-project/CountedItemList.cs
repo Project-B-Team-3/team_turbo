51bc3f6cc37a64c3c5fe20137926c158b1f0b08b
Luuk <luuk.volkering@gmail.com>
2023-02-14 12:59:19 +0100

    Made all objects for world together with hengyi

--- Mini-project/CountedItemList.cs
+++ Mini-project/CountedItemList.cs
@@ -8,4 +8,15 @@ public class CountedItemList
     {
         this.TheCountedItemList = new List<CountedItem>();
     }
+
+    public void AddCountedItem(CountedItem item)
+    {
+        this.TheCountedItemList.Add(item);
+    }
+
+    //TODO Make it so that it adds 1 to the counter when an item is already in the list.
+    public void AddItem(Item item)
+    {
+        this.TheCountedItemList.Add(new CountedItem(item, 1));
+    }
 }
---
+++ Mini-project/Player.cs
@@ -0,0 +1,15 @@
+namespace Mini_project;
+
+public class Player
+{
+    public string Name;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+    public int Gold;
+    public int ExperiencePoints;
+    public int Level;
+    public Weapon CurrentWeapon;
+    public Location CurrentLocation;
+    public QuestList QuestLog;
+    public CountedItemList Inventory;
+}
