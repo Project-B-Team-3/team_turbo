073f36e39be15dfe1bed0e8d3e0ab2a336aa890b
Luuk <luuk.volkering@gmail.com>
2023-02-15 19:01:56 +0100

    Started working on the game menu

--- Mini-project/Program.cs
+++ Mini-project/Program.cs
@@ -7,10 +7,18 @@ internal class Program
 		var gameRunning = true;
 		while (gameRunning)
 		{
+			var sAdventure = new SuperAdventure();
 			Console.WriteLine("What would you like to do? \n[Q] Quit\n[?] todo");
 			var choice = Console.ReadLine()!.ToUpper();
 			switch (choice)
 			{
+				case "M":
+					Console.WriteLine($"You are currently at {sAdventure.ThePlayer.CurrentLocation})");
+					//move
+					break;
+				case "F":
+					//fight
+					break;
 				case "Q":
 					Console.WriteLine("Game has been quit");
 					gameRunning = false;
