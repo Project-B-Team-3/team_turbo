8d891fcb7b8616645ddda20e93bdff0ad221f727
Luuk <luuk.volkering@gmail.com>
2023-02-16 16:26:09 +0100

    Fixed the problems

--- Mini-project/SuperAdventure.cs
+++ Mini-project/SuperAdventure.cs
@@ -1,14 +1,9 @@
-// using Mini_project.Classes;
-//
-// namespace Mini_project;
-//
-// public class SuperAdventure
-// {
-// 	public Player ThePlayer;
-// 	public Monster? CurrentMonster;
-//
-// 	public SuperAdventure()
-// 	{
-// 		ThePlayer = new Player("chosen one", 100, 100, 0, 0, 1, null, World.Locations[0], null, null);
-// 	}
-// }
+using Mini_project.Classes;
+
+namespace Mini_project;
+
+public class SuperAdventure
+{
+	public Player? ThePlayer;
+	public Monster? CurrentMonster;
+}
