de83460b3d01af89d40bd46315e23c86fa48ca1d
hengyi2002 <57900493+hengyi2002@users.noreply.github.com>
2023-02-16 11:16:24 +0100

    + in MoveToLocation

--- Mini-project/Program.cs
+++ Mini-project/Program.cs
@@ -3,8 +3,9 @@
 namespace Mini_project;
 
 public class Program {
-    public static void DisplayMenu()
+    public static void Main()
     {
+        Player player = new Player("chosen one", 100, 100, 0, 0, 1, null, World.Locations[0], null, null);
         Console.WriteLine("1. Move North");
         Console.WriteLine("2. Move East");
         Console.WriteLine("3. Move South");
@@ -12,22 +13,29 @@ public class Program {
         Console.WriteLine("5. Load game");
         Console.WriteLine("6. Quit");
     }
-    public static void ActionsMenu(int actions)
+    public static void ActionsMenu(int actions, Player player)
     {
         switch (actions)
         {
-            case 1: 
+            case 1:
+                MoveToLocation(player, player.CurrentLocation.LocationToNorth);
                 break;
-            case 2: 
-                break;    
-            case 3: 
-                break;    
-            case 4: 
-                break;    
+            case 2:
+                MoveToLocation(player, player.CurrentLocation.LocationToEast);
+                break;
+            case 3:
+                MoveToLocation(player, player.CurrentLocation.LocationToSouth);
+                break;
+            case 4:
+                MoveToLocation(player, player.CurrentLocation.LocationToWest);
+                break;
+            case 5:
+                Quit();
+                break;
         }
-
     }
-    public static void MoveToLocation(Location location)
+
+    public static void MoveToLocation(Player player, Location location)
     {
         if (location == null)
         {
@@ -35,25 +43,19 @@ public class Program {
         }
         else
         {
-            Player.CurrentLocation = location;
+            player.CurrentLocation = location;
             Console.WriteLine("You have moved to " + location.Name);
             Console.WriteLine(location.Description);
             
-            if (location.QuestAvailableHere != null && !Player.HasQuest(location.QuestAvailableHere))
-            {
-
-            }
-            if (location.MonsterLivingHere != null)
-            {
-
-            }
+            // if (location.QuestAvailableHere != null && !Player.HasQuest(location.QuestAvailableHere))
+            // {
+            //
+            // }
+            // if (location.MonsterLivingHere != null)
+            // {
+            //
+            // }
         }
-    }
-    public static Player Player1 { get; set; }
-
-    static void Main(string[] args)
-    {
-        Player1 = new Player("chosen one", 100, 100, 0, 0, 1, null, World.Locations[0], 0, 0);
     }
     public static void Quit()
     {
