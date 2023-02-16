d52d73ede2db296595ae562743e47ff8db33fe65
hengyi2002 <57900493+hengyi2002@users.noreply.github.com>
2023-02-16 11:16:24 +0100

    + in MoveToLocation

--- Mini-project/SuperAdventure.cs
+++ Mini-project/SuperAdventure.cs
@@ -1,14 +1,14 @@
-using Mini_project.Classes;
-
-namespace Mini_project;
-
-public class SuperAdventure
-{
-	public Player ThePlayer;
-	public Monster? CurrentMonster;
-
-	public SuperAdventure()
-	{
-		ThePlayer = new Player();
-	}
-}
+// using Mini_project.Classes;
+//
+// namespace Mini_project;
+//
+// public class SuperAdventure
+// {
+// 	public Player ThePlayer;
+// 	public Monster? CurrentMonster;
+//
+// 	public SuperAdventure()
+// 	{
+// 		ThePlayer = new Player("chosen one", 100, 100, 0, 0, 1, null, World.Locations[0], null, null);
+// 	}
+// }
