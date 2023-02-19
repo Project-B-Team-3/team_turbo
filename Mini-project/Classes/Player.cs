bce5bf29bcbcb3b1b9465c5695024f6ce9e0f4f4
DevTyldor <120493251+DevTyldor@users.noreply.github.com>
2023-02-19 15:30:26 +0100

    Update player.cs
    Added healing functionality.

--- Mini-project/Classes/Player.cs
+++ Mini-project/Classes/Player.cs
@@ -1,35 +1,45 @@
-namespace Mini_project.Classes;
-
-public class Player
+namespace Mini_project.Classes
 {
-	public string Name;
-	public int CurrentHitPoints;
-	public int MaximumHitPoints;
-	public int Gold;
-	public int ExperiencePoints;
-	public int Level;
-	public Weapon CurrentWeapon;
-	public Location CurrentLocation;
-	public QuestList QuestLog;
-	public CountedItemList Inventory;
+    public class Player
+    {
+        public string Name;
+        public int CurrentHitPoints;
+        public int MaximumHitPoints;
+        public int Gold;
+        public int ExperiencePoints;
+        public int Level;
+        public Weapon CurrentWeapon;
+        public Location CurrentLocation;
+        public QuestList QuestLog;
+        public CountedItemList Inventory;
 
-	public Player(string name, int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level, Weapon currentWeapon, Location currentLocation, QuestList questLog, CountedItemList inventory)
-	{
-		Name = name;
-		CurrentHitPoints = currentHitPoints;
-		MaximumHitPoints = maximumHitPoints;
-		Gold = gold;
-		ExperiencePoints = experiencePoints;
-		Level = level;
-		CurrentWeapon = currentWeapon;
-		CurrentLocation = currentLocation;
-		QuestLog = questLog;
-		Inventory = inventory;
-	}
+        public Player(string name, int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level, Weapon currentWeapon, Location currentLocation, QuestList questLog, CountedItemList inventory)
+        {
+            Name = name;
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+            Gold = gold;
+            ExperiencePoints = experiencePoints;
+            Level = level;
+            CurrentWeapon = currentWeapon;
+            CurrentLocation = currentLocation;
+            QuestLog = questLog;
+            Inventory = inventory;
+        }
 
-	public override string ToString()
-	{
-		return $"Your name is {Name} and you have {CurrentHitPoints} lives left!";
-	}
+        public override string ToString()
+        {
+            return $"Your name is {Name} and you have {CurrentHitPoints} lives left!";
+        }
+
+        public void Heal(int amount)
+        {
+            CurrentHitPoints += amount;
+
+            if (CurrentHitPoints > MaximumHitPoints)
+            {
+                CurrentHitPoints = MaximumHitPoints;
+            }
+        }
+    }
 }
-
