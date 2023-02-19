7724f4788770624be7dbebf5d8e194b544980200
DevTyldor <120493251+DevTyldor@users.noreply.github.com>
2023-02-19 20:00:57 +0100

    Update CountedItemList.cs
    Adding Add/RemoveItem options so I can properly finish items/gold etc from fighting. 
    UseItem will be reserved for Player.cs since it makes more sense there in my opinion.

--- Mini-project/Classes/CountedItemList.cs
+++ Mini-project/Classes/CountedItemList.cs
@@ -2,25 +2,47 @@ namespace Mini_project.Classes;
 
 public class CountedItemList
 {
-	public List<CountedItem> TheCountedItemList;
+    public List<CountedItem> TheCountedItemList;
 
-	public CountedItemList()
-	{
-		TheCountedItemList = new List<CountedItem>();
-	}
+    public CountedItemList()
+    {
+        TheCountedItemList = new List<CountedItem>();
+    }
 
-	public void AddCountedItem(CountedItem item)
-	{
-		TheCountedItemList.Add(item);
-	}
+    public void AddCountedItem(CountedItem item)
+    {
+        TheCountedItemList.Add(item);
+    }
 
-	public void AddItem(Item item)
-	{
-		if (TheCountedItemList.Any(h => item == h.TheItem))
-		{
-			TheCountedItemList.First(h => item == h.TheItem).Quantity += 1;
-		}else{
-			TheCountedItemList.Add(new CountedItem(item, 1));
-		}
-	}
+    public void AddItem(Player player, Item item)
+    {
+        if (TheCountedItemList.Any(h => item == h.TheItem))
+        {
+            TheCountedItemList.First(h => item == h.TheItem).Quantity += 1;
+        }
+        else
+        {
+            TheCountedItemList.Add(new CountedItem(item, 1));
+        }
+
+        // Add item to player's inventory
+        player.AddItem(item);
+    }
+
+    public void RemoveItem(Player player, Item item)
+    {
+        var itemToRemove = TheCountedItemList.FirstOrDefault(h => item == h.TheItem);
+
+        if (itemToRemove != null)
+        {
+            itemToRemove.Quantity -= 1;
+            if (itemToRemove.Quantity == 0)
+            {
+                TheCountedItemList.Remove(itemToRemove);
+            }
+
+            // Remove item from player's inventory
+            player.RemoveItem(item);
+        }
+    }
 }
