ba1164b7eaf39a59b2cba250f3b9253931c38cbc
DevTyldor <120493251+DevTyldor@users.noreply.github.com>
2023-02-19 16:45:03 +0100

    Update Monster.cs
    Adding ScrumBoard item: When attacking, the player and monsters do a variable amount of damage

--- Mini-project/Classes/Monster.cs
+++ Mini-project/Classes/Monster.cs
@@ -2,28 +2,34 @@ namespace Mini_project.Classes;
 
 public class Monster
 {
-	public int Id;
-	public string Name;
-	public string NamePlural;
-	public int MaximumDamage;
-	public int MinimumDamage;
-	public int? RewardExperience;
-	public int? RewardGold;
-	public CountedItemList Loot;
-	public int CurrentHitPoints;
+    public int Id;
+    public string Name;
+    public string NamePlural;
+    public int MaximumDamage;
+    public int MinimumDamage;
+    public int? RewardExperience;
+    public int? RewardGold;
+    public CountedItemList Loot;
+    public int CurrentHitPoints;
+    private Random _random = new Random();
 
-	public Monster(int id, string name, string nameplural, int maximumdamage, int minimumdamage, int rewardexperience,
-		int rewardgold, int currenthitpoints)
-	{
-		Id = id;
-		Name = name;
-		NamePlural = nameplural;
-		MaximumDamage = maximumdamage;
-		MinimumDamage = minimumdamage;
-		RewardExperience = rewardexperience;
-		RewardGold = rewardgold;
-		CurrentHitPoints = currenthitpoints;
+    public Monster(int id, string name, string nameplural, int maximumdamage, int minimumdamage, int rewardexperience,
+        int rewardgold, int currenthitpoints)
+    {
+        Id = id;
+        Name = name;
+        NamePlural = nameplural;
+        MaximumDamage = maximumdamage;
+        MinimumDamage = minimumdamage;
+        RewardExperience = rewardexperience;
+        RewardGold = rewardgold;
+        CurrentHitPoints = currenthitpoints;
 
-		Loot = new CountedItemList();
-	}
+        Loot = new CountedItemList();
+    }
+
+    public int Attack()
+    {
+        return _random.Next(MaximumDamage + 1);
+    }
 }
