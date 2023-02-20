48813aeed6b46cec52d130cbe128893babe5bc40
Thijsvbl <124798574+Thijsvbl@users.noreply.github.com>
2023-02-20 16:12:42 +0100

    Monsters tegenkomen is mogelijk

--- Mini-project/Program.cs
+++ Mini-project/Program.cs
@@ -4,8 +4,75 @@ namespace Mini_project;
 
 public class Program
 {
-	private static readonly Player ThePlayer = new("Placeholder", 100, 100, 0, 0, 1,
-		World.WeaponByID(0), World.Locations[0], new QuestList(), new CountedItemList());
+    private static readonly Player ThePlayer = new ("Placeholder", 100, 100, 0, 0, 1,
+        World.WeaponByID(0), World.Locations[0], new QuestList(), new CountedItemList());
+    public static void Main()
+    {
+        Console.WriteLine("What do you want your name to be?");
+        ThePlayer.Name = Console.ReadLine()!;
+        if (ThePlayer.Name == "")
+        {
+            ThePlayer.Name = "The chosen one";
+        }
+        var gameRunning = true;
+        while (gameRunning)
+        {
+            Console.WriteLine("What would you like to do? \n[S] See stats\n[M] Move\n[F] Fight\n[Q] Quit");
+            var choice = Console.ReadLine()!.ToUpper();
+            switch (choice)
+            {
+                case "S":
+                    Console.WriteLine(ThePlayer.ToString());
+                    break;
+                case "M":
+                    Console.WriteLine("What direction do you want to move in?");
+                    Console.WriteLine("1. Move North");
+                    Console.WriteLine("2. Move East");
+                    Console.WriteLine("3. Move South");
+                    Console.WriteLine("4. Move West");
+                    Console.WriteLine("5. Go back");
+                    ActionsMenu(int.Parse(Console.ReadLine()!));
+                    break;
+                case "F":
+                    if (ThePlayer.CurrentLocation.MonsterLivingHere != null)
+                    {
+                        Console.WriteLine($"You have encountered a {ThePlayer.CurrentLocation.MonsterLivingHere.Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("There's no monster to fight!");
+                    }
+                    break;
+                case "Q":
+                    Console.WriteLine("Game has been quit");
+                    gameRunning = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option, Make sure to choose from the above.");
+                    break;
+            }
+        }
+    }
+    private static void ActionsMenu(int actions)
+    {
+        switch (actions)
+        {
+            case 1:
+                MoveToLocation(ThePlayer.CurrentLocation.LocationToNorth);
+                break;
+            case 2:
+                MoveToLocation(ThePlayer.CurrentLocation.LocationToEast);
+                break;
+            case 3:
+                MoveToLocation(ThePlayer.CurrentLocation.LocationToSouth);
+                break;
+            case 4:
+                MoveToLocation(ThePlayer.CurrentLocation.LocationToWest);
+                break;
+            case 5:
+                return;
+        }
+    }
 
 	public static void Main()
 	{
