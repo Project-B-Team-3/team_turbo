8faa513c91f4f3036e6117d0a6e9566a3b6c8fac
Luuk <luuk.volkering@gmail.com>
2023-02-22 14:40:31 +0100

    Added readline for name of player back in

--- Mini-project/Program.cs
+++ Mini-project/Program.cs
@@ -9,6 +9,8 @@ public class Program
     public static void Main()
     {
         Console.WriteLine("What do you want your name to be?");
+
+        ThePlayer.Name = Console.ReadLine()!;
         
         if (ThePlayer.Name == "")
         {
