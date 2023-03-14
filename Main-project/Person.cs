72feef26d749fd7575415484ea9f6e3a1448dc82
Luuk <luuk.volkering@gmail.com>
2023-03-14 15:45:34 +0100

    Started with the different classes

---
+++ Main-project/Person.cs
@@ -0,0 +1,13 @@
+namespace Main_project;
+
+public class Person
+{
+	private string _name;
+	private DateOnly _birthday;
+
+	public Person(string name, DateOnly birthday)
+	{
+		_name = name;
+		_birthday = birthday;
+	}
+}
