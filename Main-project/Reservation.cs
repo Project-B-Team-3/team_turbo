657fbc76fc7760f17d7eca1cb45004467cf7fc85
Luuk <luuk.volkering@gmail.com>
2023-03-21 15:45:48 +0100

    Added reservation number

--- Main-project/Reservation.cs
+++ Main-project/Reservation.cs
@@ -1,13 +1,17 @@
+using System.Security.Cryptography;
+
 namespace Main_project;
 
 public class Reservation
 {
+	private int _number;
 	private Flight _flight;
 	private List<Person> _person;
 
-	public Reservation(Flight flight, List<Person> person)
+	public Reservation(int number, Flight flight, List<Person> person)
 	{
 		_flight = flight;
 		_person = person;
+		_number = number;
 	}
 }
