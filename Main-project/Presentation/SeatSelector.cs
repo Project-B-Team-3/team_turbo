cee271bfdca4d974fe79fd32edcd28c0443a8e1f
Luuk <luuk.volkering@gmail.com>
2023-03-23 14:00:46 +0100

    Started work on the seatselector

---
+++ Main-project/Presentation/SeatSelector.cs
@@ -0,0 +1,10 @@
+namespace Main_project.Presentation;
+
+public class SeatSelector
+{
+	public void SelectSeat()
+	{
+		Console.Clear();
+		Console.WriteLine("You can select your seat under here:");
+	}
+}
