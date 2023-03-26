41482c863b49e2b01aef99198f965376b97a111a
DevTyldor <mennonm20@gmail.com>
2023-03-26 19:11:52 +0200

    Edit flight datastructure, add json data generator

--- Main-project/DataModels/Flight.cs
+++ Main-project/DataModels/Flight.cs
@@ -5,11 +5,13 @@ namespace Main_project.DataModels
     public class Flight
     {
         public string FlightNumber { get; set; }
-        public string Departure { get; set; }
-        public string Destination { get; set; }
+        public string DepartureAirportCode { get; set; } 
+        public string DestinationAirportCode { get; set; }
         public DateTime DepartureTime { get; set; }
         public List<Seat> Seats { get; set; }
         public decimal Price { get; set; }
+        public string DepartureCity { get; set; }
+        public string DestinationCity { get; set; }
 
         public Flight(string number, string departure, string destination, DateTime departuretime, int seatCount, int premiumCount, decimal price)
         {
