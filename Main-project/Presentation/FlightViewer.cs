c426f407bbe1a7992796c7f4d5bedc65fcec4c72
Luuk <luuk.volkering@gmail.com>
2023-03-26 20:02:44 +0200

    Ongebruikte code verwijdert

--- Main-project/Presentation/FlightViewer.cs
+++ Main-project/Presentation/FlightViewer.cs
@@ -1,5 +1,4 @@
 using Main_project.Logic;
-using Main_project.DataModels;
 
 namespace Main_project.Presentation
 {
@@ -22,15 +21,5 @@ namespace Main_project.Presentation
                 Console.ReadKey();
             }
         }
-
-        // public int GetFlightSelection()
-        // {
-        //     int selection = 0;
-        //     do
-        //     {
-        //         Console.Write("Enter the number of the flight you want to book: ");
-        //     } while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > flights.Count);
-        //     return selection;
-        // }
     }
 }
