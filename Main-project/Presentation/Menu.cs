ee630282caf43a49b1c7e83552e54b95b848f96f
Luuk <luuk.volkering@gmail.com>
2023-03-27 13:28:14 +0200

    Had ik klein beetje verkeerd bewerkt, maar nu ist goed.

--- Main-project/Presentation/Menu.cs
+++ Main-project/Presentation/Menu.cs
@@ -11,50 +11,44 @@ public class Menu
         ConsoleKeyInfo key = new ConsoleKeyInfo();
         while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape &&  x == false)
         {
-            bool adminpanelrunning = false;
-            bool x = false;
-            ConsoleKeyInfo key = new ConsoleKeyInfo();
-            while (!Console.KeyAvailable && key.Key != ConsoleKey.Escape &&  x == false)
+            Console.Clear();
+            Console.WriteLine("What do you wish to do?\n[1] View all upcoming flights\n[2] Book a flight\n[3] Cancel a flight\n[4] Quit\n");
+            
+            key = Console.ReadKey(true);
+            //Press key to trigger event ( D0 = 0 , D1 = 1 etc.)
+            switch (key.Key)
             {
-                Console.Clear();
-                Console.WriteLine("What do you wish to do?\n[1] View all upcoming flights\n[2] Book a flight\n[3] Cancel a flight\n[4] Quit\n");
-                
-                key = Console.ReadKey(true);
-                //Press key to trigger event ( D0 = 0 , D1 = 1 etc.)
-                switch (key.Key)
-                {
-                    case ConsoleKey.D1:
-                        ConsoleView.DisplayFlights();
-                        break;
-                    case ConsoleKey.D2:
-                        Console.WriteLine("Book a flight");
-                        break;
+                case ConsoleKey.D1:
+                    ConsoleView.DisplayFlights();
+                    break;
+                case ConsoleKey.D2:
+                    Console.WriteLine("Book a flight");
+                    break;
 
-                    case ConsoleKey.D3:
-                        Console.WriteLine("Cancel a flight");
-                        break;
+                case ConsoleKey.D3:
+                    Console.WriteLine("Cancel a flight");
+                    break;
 
-                    case ConsoleKey.D4: case ConsoleKey.Q:
-                        Console.WriteLine("Program has been quit");
-                        x = true;
-                        break;
+                case ConsoleKey.D4: case ConsoleKey.Q:
+                    Console.WriteLine("Program has been quit");
+                    x = true;
+                    break;
 
-                    case ConsoleKey.S:
-                        AdminPanel.Admin();
-                        break;
-                        
-                    default:
-                        // Checks for capslock/numlock
-                        if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                case ConsoleKey.S:
+                    AdminPanel.Admin();
+                    break;
+                    
+                default:
+                    // Checks for capslock/numlock
+                    if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                    {
+                        if (Console.CapsLock && Console.NumberLock)
                         {
-                            if (Console.CapsLock && Console.NumberLock)
-                            {
-                                Console.WriteLine(key.KeyChar);
-                                Console.Write("Invalid option");
-                            }
+                            Console.WriteLine(key.KeyChar);
+                            Console.Write("Invalid option");
                         }
-                        break;
-                }
+                    }
+                    break;
             }
         }
     }
