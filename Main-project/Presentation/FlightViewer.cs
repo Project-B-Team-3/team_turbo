f4f0431d6bbb24b43e2e094fa8ce2f548ab597fe
DevTyldor <120493251+DevTyldor@users.noreply.github.com>
2023-03-27 07:37:58 +0200

    Update FlightViewer.cs
    Deleted commented code since it's irrelevant code for our project's scope at this point.

--- Main-project/Presentation/FlightViewer.cs
+++ Main-project/Presentation/FlightViewer.cs
@@ -29,15 +29,5 @@ namespace Main_project.Presentation
                 Console.ReadKey();
             }
         }
-
-        // public int GetFlightSelection()
-        // {
-        //     int selection = 0;
-        //     do
-        //     {
-        //         Console.Write("Enter the number of the flight you want to book: ");
-        //     } while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > flights.Count);
-        //     return selection;
-        // }
     }
 }
