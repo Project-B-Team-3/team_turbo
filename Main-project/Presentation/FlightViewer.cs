a6d1d9a6cc6619bf11f56de6a19e71745866f930
DevTyldor <mennonm20@gmail.com>
2023-04-04 01:22:50 +0200

    created SelectFlight, ToString fix in Flight

--- Main-project/Presentation/FlightViewer.cs
+++ Main-project/Presentation/FlightViewer.cs
@@ -1,3 +1,4 @@
+using Main_project.DataAccess;
 using Main_project.Logic;
 
 namespace Main_project.Presentation
@@ -36,14 +37,51 @@ namespace Main_project.Presentation
             }
         }
 
-        // public int GetFlightSelection()
-        // {
-        //     int selection = 0;
-        //     do
-        //     {
-        //         Console.Write("Enter the number of the flight you want to book: ");
-        //     } while (!int.TryParse(Console.ReadLine(), out selection) || selection < 1 || selection > flights.Count);
-        //     return selection;
-        // }
+        public static void SelectFlight()
+        {
+            // Get list of upcoming flights
+            var flights = BookingLogic.UpComingFlights();
+
+            // If no upcoming flights, display message and return
+            if (!flights.Any())
+            {
+                Console.WriteLine("No upcoming flights!");
+                Console.ReadKey();
+                return;
+            }
+
+            // Display list of flights for user to select from
+            Console.WriteLine("Select a flight:");
+
+            for (var i = 0; i < flights.Count(); i++)
+            {
+                Console.WriteLine($"{i + 1}: {flights.ElementAt(i).FlightNumber}");
+            }
+
+            // Get user input for flight selection
+            Console.Write("Enter the number of the flight you want to select: ");
+            var input = Console.ReadLine();
+
+            // If input is valid, get selected flight from FlightDataAccess and display selected flight
+            if (int.TryParse(input, out var index) && index >= 1 && index <= flights.Count())
+            {
+                var flight = FlightDataAccess.GetFlights().FirstOrDefault(f => f.FlightNumber == flights.ElementAt(index - 1).FlightNumber);
+                if (flight != null)
+                {
+                    Console.WriteLine($"Selected flight: {flight.FlightNumber}");
+                    // @@@TO DO@@@ Add code here later to actually book the flight? Or do something with the selected flight?
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid flight number.");
+                }
+            }
+            else // If input is not valid, display error message
+            {
+                Console.WriteLine("Invalid input. Please enter a valid flight number.");
+            }
+
+            Console.ReadKey();
+        }
     }
 }
