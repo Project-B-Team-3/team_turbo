a7c04442d4dde3c6c8aa375856414e02d01118d6
Luuk <luuk.volkering@gmail.com>
2023-04-18 16:36:33 +0200

    Pretty much finished the booking system

--- Main-project/DataModels/Person.cs
+++ Main-project/DataModels/Person.cs
@@ -5,4 +5,11 @@ public class Person
 	public string Name { get; set; }
 	public string Birthdate { get; set; }
 	public string DocumentNum { get; set; }
+
+	public Person(string name, string birthDate, string documentNum)
+	{
+		Name = name;
+		Birthdate = birthDate;
+		DocumentNum = documentNum;
+	}
 }
