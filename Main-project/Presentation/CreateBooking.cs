1187c1f902176ceaebc382f89f9515e33d0038a5
Luuk <luuk.volkering@gmail.com>
2023-04-18 16:40:04 +0200

    Added check for invalid flight number

--- Main-project/Presentation/CreateBooking.cs
+++ Main-project/Presentation/CreateBooking.cs
@@ -10,6 +10,12 @@ public static class CreateBooking
 		Console.Clear();
 		Console.WriteLine("Welcome to the booking menu, please enter a flight number to book a flight.");
 		var flightNum = Console.ReadLine();
+		if (FlightDataAccess.GetFlights().All(h => h.FlightNumber != flightNum))
+		{
+			Console.WriteLine("This flight does not exist...");
+			Thread.Sleep(200);
+			return;
+		}
 		Console.WriteLine("For how many people would you like to book?");
 		var peopleCount = Console.ReadLine();
 		var peopleInt = 0;
