7dab8b5eb2d99978bb17b6e60b0857df65868341
Luuk <luuk.volkering@gmail.com>
2023-04-18 14:13:07 +0200

    PBT3-43

--- Main-project/DataModels/Booking.cs
+++ Main-project/DataModels/Booking.cs
@@ -1,8 +1,17 @@
+using Main_project.DataAccess;
+
 namespace Main_project.DataModels;
 
 public class Booking
 {
 	public string FlightNumber { get; set; }
-	public string ReservationNumber { get; set; }
+	public int ReservationNumber { get; set; }
 	public List<string> Seats { get; set; }
+
+	public Booking(string flightNumber, string reservationNumber, List<string> seats)
+	{
+		FlightNumber = flightNumber;
+		ReservationNumber = BookingDataAccess.GetBookings().Any() ? BookingDataAccess.GetBookings().Max(h => h.ReservationNumber) + 1 : 1;
+		Seats = seats;
+	}
 }
