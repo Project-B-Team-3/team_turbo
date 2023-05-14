558fd97d0e30970e130a294cc958210abc95c4cd
DevTyldor <mennonm20@gmail.com>
2023-05-14 14:22:32 +0200

    Added catering skeleton

---
+++ Main-project/Logic/CateringLogic.cs
@@ -0,0 +1,18 @@
+﻿using Main_project.DataModels;
+
+namespace Main_project.Logic
+{
+    public class CateringLogic
+    {
+        public static List<Catering> cateringList() // I will add more catering options later after PO interview -Menno
+        {
+            var items = new List<Catering>()
+            {
+                new Catering("Chicken", "Tasty chicken", 8.50M, true),
+                new Catering("Pasta", "Tasty pasta", 10.50M, true)
+            };
+
+            return items;
+        }
+    }
+}
