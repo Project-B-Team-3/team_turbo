8db554d666093aa53961d7405a217a4ed6c01496
Luuk <luuk.volkering@gmail.com>
2023-05-29 21:37:38 +0200

    Not working first test

--- Main-project_Tests/UnitTest1.cs
+++ Main-project_Tests/UnitTest1.cs
@@ -1,11 +1,24 @@
+using Main_project.DataAccess;
+using Main_project.DataModels;
+using Main_project.Misc;
+
 namespace Main_project_Tests;
 
 [TestClass]
 public class UnitTest1
 {
-	[TestMethod]
-	public void TestMethod1()
+	public UnitTest1()
 	{
-		Assert.IsTrue(1 == 1);
+		//if(!File.Exists("../Main-project/DataSources/Airplanes/Boeing737-700.json")) AirplaneDataAccess.MakePlane(new Airplane("Boeing", "737-700", 6, 106, 6, 20));
+		//if(!File.Exists("../Main-project/DataSources/Airplanes/EmbraerE-175.json")) AirplaneDataAccess.MakePlane(new Airplane("Embraer", "E-175", 4, 60, 8, 20));
+	}
+	[TestMethod]
+	public void TestFlights()
+	{
+		if (!File.Exists("../Main-project/DataSources/Flights.json"))
+		{
+			FlightGenerator.GenerateFlights();
+		}
+		Assert.IsTrue(FlightDataAccess.GetFlights().Count > 0);
 	}
 }
