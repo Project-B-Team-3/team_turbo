1447b3d4d0d6cbace61164d863d385572f0e479e
DevTyldor <mennonm20@gmail.com>
2023-06-01 12:46:03 +0200

    fixed generate reservation code

--- Main-project/DataAccess/BookingDataAccess.cs
+++ Main-project/DataAccess/BookingDataAccess.cs
@@ -1,77 +1,52 @@
 using Main_project.DataModels;
 using Newtonsoft.Json;
 
-namespace Main_project.DataAccess;
-
-public static class BookingDataAccess
+namespace Main_project.DataAccess
 {
-	public static List<Booking> GetBookings()
-	{
-		var path = "./DataSources/Bookings.json";
-		if (!File.Exists(path))
-		{
-			File.Create(path).Close();
-		}
+    public static class BookingDataAccess
+    {
+        public static List<Booking> GetBookings()
+        {
+            try
+            {
+                var json = File.ReadAllText("./DataSources/Bookings.json");
+                var bookings = JsonConvert.DeserializeObject<List<Booking>>(json);
+                return bookings ?? new List<Booking>();
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e);
+                Environment.Exit(1);
+                return new List<Booking>();
+            }
+        }
 
-		var json = File.ReadAllText(path);
-		var bookings = JsonConvert.DeserializeObject<List<Booking>>(json);
-		return bookings ?? new List<Booking>();
-	}
+        public static void CreateBooking(Booking booking)
+        {
+            var newBookings = GetBookings();
+            newBookings.Add(booking);
+            File.WriteAllText(
+                "./DataSources/Bookings.json",
+                JsonConvert.SerializeObject(newBookings, Formatting.Indented)
+            );
+        }
 
-	public static void CreateBooking(Booking booking)
-	{
-		if (!IsValidReservationCode(booking.ReservationNumber))
-		{
-			Console.WriteLine("Invalid reservation code.");
-			return;
-		}
-
-		var newBookings = GetBookings();
-		newBookings.Add(booking);
-		File.WriteAllText(
-			"./DataSources/Bookings.json",
-			JsonConvert.SerializeObject(newBookings, Formatting.Indented)
-		);
-		File.WriteAllLines($"./{booking.ReservationNumber}.txt", booking.GetLines());
-	}
-
-	public static void RemoveBooking(Booking booking)
-	{
-		var newBookings = GetBookings();
-		if (newBookings.RemoveAll(h => h.ReservationNumber == booking.ReservationNumber) == 1)
-			Console.WriteLine("Successfully removed booking!");
-		else
-			Console.WriteLine("Could not find booking!");
-		File.WriteAllText(
-			"./DataSources/Bookings.json",
-			JsonConvert.SerializeObject(newBookings, Formatting.Indented)
-		);
-	}
-
-	private static bool IsValidReservationCode(string reservationCode)
-	{
-		if (reservationCode.Length < 8)
-			return false;
-
-		var containsLetters = false;
-		var containsNumbers = false;
-		var containsCapitalizedLetters = false;
-		var containsUncapitalizedLetters = false;
-
-		foreach (var c in reservationCode)
-			if (char.IsLetter(c))
-			{
-				containsLetters = true;
-				if (char.IsUpper(c))
-					containsCapitalizedLetters = true;
-				else
-					containsUncapitalizedLetters = true;
-			}
-			else if (char.IsDigit(c))
-			{
-				containsNumbers = true;
-			}
-
-		return containsLetters && containsNumbers && containsCapitalizedLetters && containsUncapitalizedLetters;
-	}
+        public static void RemoveBooking(Booking booking)
+        {
+            var newBookings = GetBookings();
+            if (newBookings.RemoveAll(h => h.ReservationNumber == booking.ReservationNumber) == 1)
+            {
+                Console.WriteLine("Successfully removed booking!");
+            }
+            else
+            {
+                Console.WriteLine("Could not find booking!");
+            }
+            File.WriteAllText(
+                "./DataSources/Bookings.json",
+                JsonConvert.SerializeObject(newBookings, Formatting.Indented)
+            );
+        }
+    }
 }
--- Main-project/DataModels/Booking.cs
+++ Main-project/DataModels/Booking.cs
@@ -1,55 +1,23 @@
-using System.Text.RegularExpressions;
-using Main_project.DataAccess;
-
-namespace Main_project.DataModels;
-
-public class Booking
+namespace Main_project.DataModels
 {
-	private string reservationNumber;
+    public class Booking
+    {
+        public string ReservationNumber { get; set; }
+        public string FlightNumber { get; set; }
+        public Dictionary<string, Person> Seats { get; set; }
+        public Cost Cost { get; set; }
 
-	public string ReservationNumber
-	{
-		get => reservationNumber;
-		set
-		{
-			if (!IsValidReservationCode(value)) throw new ArgumentException("Invalid reservation code.");
-			reservationNumber = value;
-		}
-	}
-
-	public string FlightNumber { get; set; }
-	public Dictionary<string, Person> Seats { get; set; }
-	public Cost Cost { get; set; }
-
-	public Booking(
-		string reservationNumber,
-		string flightNumber,
-		Dictionary<string, Person> seats,
-		Cost cost
-	)
-	{
-		ReservationNumber = reservationNumber;
-		FlightNumber = flightNumber;
-		Seats = seats;
-		Cost = cost;
-	}
-
-
-	public string[] GetLines()
-	{
-		Flight flight = FlightDataAccess.GetFlights().First(h => h.FlightNumber == FlightNumber);
-		string[] lines = {$"You just booked a flight from {flight.DepartureCity} to {flight.DestinationCity}.",
-			Seats.Values.ToArray().ToString()};
-		return lines;
-	}
-
-	private bool IsValidReservationCode(string code)
-	{
-		if (code.Length < 8)
-			return false;
-
-		var pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$";
-		var regex = new Regex(pattern);
-		return regex.IsMatch(code);
-	}
+        public Booking(
+            string reservationNumber,
+            string flightNumber,
+            Dictionary<string, Person> seats,
+            Cost cost
+        )
+        {
+            ReservationNumber = reservationNumber;
+            FlightNumber = flightNumber;
+            Seats = seats;
+            Cost = cost;
+        }
+    }
 }
