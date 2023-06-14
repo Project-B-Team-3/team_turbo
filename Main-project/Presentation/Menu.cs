347ebe8c27ea50ebfc0d4f800be101879a906957
hengyi2002 <57900493+hengyi2002@users.noreply.github.com>
2023-06-14 15:19:20 +0200

    added login

--- Main-project/Presentation/Menu.cs
+++ Main-project/Presentation/Menu.cs
@@ -14,7 +14,7 @@ public static class Menu
 		{
 			Console.Clear();
 			Console.WriteLine(
-				"What do you wish to do?\n[1] View all upcoming flights\n[2] Book a flight\n[3] Change a booking\n[4] Cancel a booking\n[5] Contact us\n[6] Quit\n"
+				"What do you wish to do?\n[1] View all upcoming flights\n[2] Book a flight\n[3] Change/cancel a booking\n[4] Contact us\n[5] Quit\n"
 			);
 
 			key = Console.ReadKey(true);
@@ -24,33 +24,21 @@ public static class Menu
 				case ConsoleKey.D1:
 					ConsoleView.DisplayFlights();
 					break;
+				
 				case ConsoleKey.D2:
 					CreateBooking.CreateNewBooking();
 					break;
+				
 				case ConsoleKey.D3:
-					// TODO implement changing booking
-					// UserLogin.Start();
-					Console.WriteLine("You will be able to change your booking here...");
+					UserLogin.Login();
 					Console.ReadKey(intercept: true);
 					break;
+				
 				case ConsoleKey.D4:
-					Console.WriteLine("Please enter your booking number:");
-					var reservationNumber = Console.ReadLine();
-					if(BookingDataAccess.GetBookings().Any(h => h.ReservationNumber == reservationNumber)){
-						ChangeBooking.CancelBooking(reservationNumber);
-						Console.WriteLine($"Your booking with number {reservationNumber} is cancelled!");
-					}
-					else
-					{
-						Console.WriteLine($"There is no booking with the number {reservationNumber}!");
-					}
-
-					Console.ReadKey(intercept: true);
-					break;
-				case ConsoleKey.D5:
 					DisplayContactDetails();
 					break;
-				case ConsoleKey.D6:
+				
+				case ConsoleKey.D5:
 				case ConsoleKey.Q:
 					Console.WriteLine("Program has been quit");
 					x = true;
