5bf8b57cbfd254389c9ddae7867f86b7dddfd05d
Luuk <luuk.volkering@gmail.com>
2023-06-15 11:52:15 +0200

    Legenda bij stoelkeuze voor kleuren

--- Main-project/Presentation/SeatSelector.cs
+++ Main-project/Presentation/SeatSelector.cs
@@ -10,6 +10,12 @@ public static class SeatSelector
 	{
 		Console.Clear();
 		Console.WriteLine("The white seats are available:");
+		Console.ForegroundColor = ConsoleColor.DarkBlue;
+		Console.WriteLine("The dark blue seats are First Class");
+		Console.ForegroundColor = ConsoleColor.Blue;
+		Console.WriteLine("The blue seats are Business Class");
+		Console.ForegroundColor = ConsoleColor.White;
+		Console.WriteLine("The white seats are Economy Class");
 		foreach (var seat in BookingLogic.FlightSeats(flightNumber))
 		{
 			Console.ForegroundColor = color(seat);
